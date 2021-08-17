cebd90f5e39625fedde6263e56d7092ca7a87b14
BORBER <borber.drum@gmail.com>
2021-08-17 16:17:55 +0800

    添加 解码

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import re
 
 url="http://mmmono.com/group/100044/"
 r = requests.get(url).content
-pic_url = re.findall('img src="(.*?)"',r,re.S)[2]
+pic_url = re.findall('img src="(.*?)"',r.decode('utf-8'),re.S)[2]
 r = requests.get(pic_url)
 with open(strftime('./P/%Y%m%d',localtime()) + '.png', 'wb') as f:
     f.write(r.content)
